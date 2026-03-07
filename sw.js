ff5ef5fc9265b12fd20e27143c4402d0b3f8904b
iamjibesh <62950135+iamjibesh@users.noreply.github.com>
2026-03-08 00:08:35 +0530

    Update service worker cache version to v1.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'basecalc-v1';
+const CACHE_NAME = 'basecalc-v1.1';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -43,3 +43,4 @@ self.addEventListener('fetch', event => {
       })
   );
 });
+
